e3cd735df16219825c861e965224a73676035e4f
sumrits <sumrits@sfu.ca>
2021-10-15 01:46:56 -0700

    Setup options button

--- app/src/main/java/com/example/cmpt276as3/MainActivity.java
+++ app/src/main/java/com/example/cmpt276as3/MainActivity.java
@@ -2,7 +2,13 @@ package com.example.cmpt276as3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.cmpt276as3.model.Options;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setupOptionButton();
+
+
     }
+
+    private void setupOptionButton() {
+        Button btn_options = (Button) findViewById(R.id.ms_options);
+        btn_options.setBackgroundColor(Color.BLACK);
+        btn_options.setTextColor(Color.WHITE);
+        btn_options.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Options.class);
+                startActivity(intent);
+            }
+        });
+    }
+
 }
---
+++ app/src/main/java/com/example/cmpt276as3/model/Options.java
@@ -0,0 +1,16 @@
+package com.example.cmpt276as3.model;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.cmpt276as3.R;
+
+public class Options extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_options);
+    }
+}
