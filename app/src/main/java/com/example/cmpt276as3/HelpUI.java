5924995ef45b6e736a7d915ee234296c9dc9b232
sumrits <sumrits@sfu.ca>
2021-10-23 22:43:58 -0700

    Help activity implemented

---
+++ app/src/main/java/com/example/cmpt276as3/HelpUI.java
@@ -0,0 +1,17 @@
+package com.example.cmpt276as3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HelpUI extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help);
+
+        //Add HyperLink
+        //https://www.youtube.com/watch?v=tL9fCKC2lNA
+    }
+}
